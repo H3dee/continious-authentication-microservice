0bad3c07b2cd68e76656a0d18266338da6faeb09
Fedor Sidelnikov <fedor.sidelnikov@altenar.com>
2022-05-18 20:59:29 +0300

    Added new classification models, removed old processing functions

--- flaskr/ML_API/API.py
+++ flaskr/ML_API/API.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import classifiers
+import processing
 
 
 def train_all(data):
@@ -12,15 +13,21 @@ def train_all(data):
     data
         The train dataframe containing data for users in the system
     """
+    try:
+        df = pd.read_csv(data)
+        users_ids = pd.unique(df['class'])
+        for user_id in users_ids:
+            rf = classifiers.RF(user_id)
+            train_df = processing.prepare_dataset(df, user_id)
+            x, y = processing.split_and_normalize_dataset(train_df, user_id)
+            params = rf.tune_params(x, y)
+            rf.train(x, y, params)
+    except (ValueError, FileNotFoundError):
+        print('User isn''t exist in the system')
 
-    df = pd.read_csv(data)
-    users_ids = pd.unique(df['class'])
-    for user_id in users_ids:
-        rf = classifiers.RF(user_id)
-        train_df = rf.prepare_dataset(df)
-        x, y = rf.split_and_normalize_dataset(train_df)
-        params = rf.tune_params(x, y)
-        rf.train(x, y, params)
+    except BaseException as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
 
 
 def validate_user(data, user_id):
@@ -36,11 +43,16 @@ def validate_user(data, user_id):
     user_id
         The user id
     """
+    try:
+        rf = classifiers.RF(user_id)
+        df = pd.read_csv(data)
+        test_df = processing.prepare_test_dataset(df, user_id)
+        x, y = processing.split_and_normalize_test_dataset(test_df, user_id)
+        is_valid = rf.predict(x, y)
+        return is_valid
+    except (ValueError, FileNotFoundError):
+        print('User isn''t exist in the system')
 
-    rf = classifiers.RF(user_id)
-    df = pd.read_csv(data)
-    test_df = rf.prepare_test_dataset(df)
-    x, y = rf.split_and_normalize_test_dataset(test_df)
-    is_valid = rf.predict(x, y)
-
-    return is_valid
+    except BaseException as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
--- flaskr/ML_API/classifiers.py
+++ flaskr/ML_API/classifiers.py
@@ -1,13 +1,13 @@
-import logging
 import pickle
-
 import numpy as np
-import pandas as pandas
-from sklearn import preprocessing
+from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.utils import shuffle
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 
 class RF:
@@ -22,73 +22,10 @@ class RF:
             clf = pickle.load(f)
         predicted = clf.predict(x)
         accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
-        self.logger.info(f'F1-Score: {accuracy}%')
+        print(f'F1-Score: {accuracy}%')
         if accuracy >= 70:
             return True
         return False
-
-    def prepare_dataset(self, dataset):
-        users_count = len(pandas.unique(dataset['class']))
-        self.logger.info(f'Users count: {users_count}')
-
-        user_features_count = len(dataset[lambda x: x['class'] == self.user_id])
-        self.logger.info(f'Features count for user id {self.user_id}: {user_features_count}')
-
-        users_ids = pandas.unique(dataset['class'])
-        users_ids = users_ids[users_ids != self.user_id]
-
-        feature_count_per_user = round(user_features_count / (users_count - 1))
-        self.logger.info(f'Feature number for the rest of the users: {feature_count_per_user}')
-        for user_id in users_ids:
-            count = len(dataset[lambda x: x['class'] == user_id])
-            if count < feature_count_per_user:
-                user_features_count = count
-                self.logger.info(f'Total feature amount per user decreased to {count}')
-
-        data = dataset[lambda x: x['class'] == self.user_id].head(user_features_count)
-
-        for user_id in users_ids:
-            data = pandas.concat([data, dataset[lambda x: x['class'] == user_id].head(feature_count_per_user)])
-        self.logger.info('Split train data')
-        data.loc[lambda x: x['class'] == self.user_id, 'class'] = 1
-        data.loc[lambda x: x['class'] != 1, 'class'] = 0
-        self.logger.info('Marked train data')
-
-        return data
-
-    def prepare_test_dataset(self, data):
-        data.loc[lambda x: x['class'] == self.user_id, 'class'] = 1
-        return data
-
-    def split_and_normalize_dataset(self, dataset):
-        dataset = shuffle(dataset)
-        y = dataset['class']
-        dataset = dataset.drop(['class', 'type_of_action'], axis=1)
-        values = dataset.values
-
-        scaler = preprocessing.StandardScaler()
-        scaled_values = scaler.fit_transform(values)
-        with open(f'ml_normalization_state_userId_{self.user_id}.pkl', 'wb') as f:
-            pickle.dump(scaler, f)
-        self.logger.info(f'Saved normalization state for user {self.user_id}')
-        df_normalized = pandas.DataFrame(scaled_values)
-        x = df_normalized
-
-        self.logger.info('Split and normalized dataset')
-
-        return x, y
-
-    def split_and_normalize_test_dataset(self, dataset):
-        y = dataset['class']
-        dataset = dataset.drop(['class', 'type_of_action'], axis=1)
-        values = dataset.values
-        with open(f'ml_normalization_state_userId_{self.user_id}.pkl', 'rb') as f:
-            scaler = pickle.load(f)
-        scaled_values = scaler.transform(values)
-        df_normalized = pandas.DataFrame(scaled_values)
-        x = df_normalized
-
-        return x, y
 
     def tune_params(self, x, y):
         n_estimators = [int(x) for x in np.linspace(start=10, stop=80, num=10)]
@@ -97,17 +34,13 @@ class RF:
         min_samples_split = [2, 5]
         min_samples_leaf = [1, 2]
         bootstrap = [True, False]
-        param_grid = {'n_estimators': n_estimators,
-                      'max_features': max_features,
-                      'max_depth': max_depth,
-                      'min_samples_split': min_samples_split,
-                      'min_samples_leaf': min_samples_leaf,
-                      'bootstrap': bootstrap}
+        param_grid = dict(n_estimators=n_estimators, max_features=max_features, max_depth=max_depth,
+                          min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, bootstrap=bootstrap)
 
-        rf_grid = GridSearchCV(estimator=RandomForestClassifier(), param_grid=param_grid, cv=4, verbose=2, n_jobs=4)
+        rf_grid = GridSearchCV(estimator=RandomForestClassifier(), scoring='f1', param_grid=param_grid, cv=5, verbose=2, n_jobs=6)
         rf_grid.fit(x, y)
 
-        self.logger.info(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
         params = rf_grid.best_params_
 
         return params
@@ -115,13 +48,186 @@ class RF:
     def __init__(self, user_id):
         self.user_id = user_id
 
-        self.logger = logging.getLogger("dev")
-        self.logger.setLevel(logging.INFO)
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s')
-        console_handler.setFormatter(formatter)
-        self.logger.addHandler(console_handler)
+    user_id = int
+
+
+class KNN:
+    def train(self, x, y, params):
+        clf = KNeighborsClassifier(**params)
+        clf = clf.fit(x, y)
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+
+    def predict(self, x, y):
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'rb') as f:
+            clf = pickle.load(f)
+        predicted = clf.predict(x)
+        accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
+        print(f'F1-Score: {accuracy}%')
+        if accuracy >= 70:
+            return True
+        return False
+
+    def tune_params(self, x, y):
+        k_range = range(1, 31, 2)
+        param_grid = dict(n_neighbors=k_range)
+
+        rf_grid = GridSearchCV(estimator=KNeighborsClassifier(), scoring='f1', param_grid=param_grid, cv=10, verbose=1,
+                               n_jobs=4)
+        rf_grid = rf_grid.fit(x, y)
+
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        params = rf_grid.best_params_
+
+        return params
+
+    def __init__(self, user_id):
+        self.user_id = user_id
 
     user_id = int
-    logger = None
+
+
+class DT:
+    def train(self, x, y, params):
+        clf = tree.DecisionTreeClassifier(**params)
+        clf = clf.fit(x, y)
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+
+    def predict(self, x, y):
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'rb') as f:
+            clf = pickle.load(f)
+        predicted = clf.predict(x)
+        accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
+        print(f'F1-Score: {accuracy}%')
+        if accuracy >= 70:
+            return True
+        return False
+
+    def tune_params(self, x, y):
+        criterion = ['gini', 'entropy']
+        max_depth = [4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 30, 40, 50, 70, 90, 120, 150]
+        param_grid = dict(criterion=criterion, max_depth=max_depth)
+
+        rf_grid = GridSearchCV(estimator=tree.DecisionTreeClassifier(), param_grid=param_grid, cv=10, verbose=1,
+                               n_jobs=4)
+        rf_grid = rf_grid.fit(x, y)
+
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        params = rf_grid.best_params_
+
+        return params
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    user_id = int
+
+
+class SVM:
+    def train(self, x, y, params):
+        clf = SVC(**params)
+        clf = clf.fit(x, y)
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+
+    def predict(self, x, y):
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'rb') as f:
+            clf = pickle.load(f)
+        predicted = clf.predict(x)
+        accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
+        print(f'F1-Score: {accuracy}%')
+        if accuracy >= 70:
+            return True
+        return False
+
+    def tune_params(self, x, y):
+        C = [0.1, 1, 10, 100, 1000]
+        gamma = [1, 0.1, 0.01, 0.001, 0.0001]
+        kernel = ['rbf']
+        param_grid = dict(C=C, gamma=gamma, kernel=kernel)
+
+        rf_grid = GridSearchCV(estimator=SVC(), param_grid=param_grid, scoring='f1', cv=4, verbose=2, n_jobs=4)
+        rf_grid = rf_grid.fit(x, y)
+
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        params = rf_grid.best_params_
+
+        return params
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    user_id = int
+
+
+class LR:
+    def train(self, x, y, params):
+        clf = LogisticRegression(**params)
+        clf = clf.fit(x, y)
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+
+    def predict(self, x, y):
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'rb') as f:
+            clf = pickle.load(f)
+        predicted = clf.predict(x)
+        accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
+        print(f'F1-Score: {accuracy}%')
+        if accuracy >= 70:
+            return True
+        return False
+
+    def tune_params(self, x, y):
+        solver = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+        penalty = ['l1', 'l2']
+        C = [0.001, 0.01, 0.1, 1, 10, 100]
+        param_grid = dict(solver=solver, penalty=penalty, C=C)
+
+        rf_grid = GridSearchCV(estimator=LogisticRegression(max_iter=500), param_grid=param_grid, scoring='f1', cv=5, verbose=2, n_jobs=4)
+        rf_grid = rf_grid.fit(x, y)
+
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        params = rf_grid.best_params_
+
+        return params
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    user_id = int
+
+
+class NB:
+    def train(self, x, y, params):
+        clf = GaussianNB(**params)
+        clf = clf.fit(x, y)
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+
+    def predict(self, x, y):
+        with open(f'ml_state_userId_{self.user_id}.pkl', 'rb') as f:
+            clf = pickle.load(f)
+        predicted = clf.predict(x)
+        accuracy = round(f1_score(y, predicted, average='macro'), 3) * 100
+        print(f'F1-Score: {accuracy}%')
+        if accuracy >= 70:
+            return True
+        return False
+
+    def tune_params(self, x, y):
+        var_smoothing = np.logspace(0, -9, num=100)
+        param_grid = dict(var_smoothing=var_smoothing)
+
+        rf_grid = GridSearchCV(estimator=GaussianNB(), param_grid=param_grid, scoring='f1', cv=10, verbose=2, n_jobs=4)
+        rf_grid = rf_grid.fit(x, y)
+
+        print(f"Scored best params for user {self.user_id}: {rf_grid.best_params_}")
+        params = rf_grid.best_params_
+
+        return params
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    user_id = int
